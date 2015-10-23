98c22f4b062fc6bec3b00836497e8cd84000d9ca
Alex <oo.shcherbakov@gmail.com>
2015-10-22 21:39:53 -0400

    Initial commit

---
+++ TimeSlower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeSlower
+//
+//  Created by Oleksandr Shcherbakov on 10/22/15.
+//  Copyright © 2015 Oleksandr Shcherbakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
