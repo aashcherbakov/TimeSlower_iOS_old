4cc65346970695daf0496e4c953229298f11c6a9
Alex <oo.shcherbakov@gmail.com>
2015-11-08 11:40:41 -0500

    Basic connection of view controllers from legacy storyboard

---
+++ TimeSlower/Features/MainNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  MainNavigationController.swift
+//  TimeSlower
+//
+//  Created by Oleksandr Shcherbakov on 11/6/15.
+//  Copyright © 2015 Oleksandr Shcherbakov. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
