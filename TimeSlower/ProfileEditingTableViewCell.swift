dc11c09f0ffadc3c70f09491e73aa75dba494df4
Alex <oo.shcherbakov@gmail.com>
2015-12-07 21:57:27 -0500

    Update profile editing cell

--- TimeSlower/ProfileEditingTableViewCell.swift
+++ TimeSlower/ProfileEditingTableViewCell.swift
@@ -11,6 +11,13 @@ import JVFloatLabeledText
 import RxCocoa
 import RxSwift
 
+/**
+ Enum of strings that descrybes cell type
+ 
+ - Name:     to enter name
+ - Country:  to select country
+ - Birthday: to select birthday
+ */
 enum ProfileEditingCellType: String {
     case Name = "name"
     case Country = "country"
@@ -19,39 +26,61 @@ enum ProfileEditingCellType: String {
 
 class ProfileEditingTableViewCell: UITableViewCell {
     
+    /**
+     Enum that describes current state of cell
+     
+     - Default: default state
+     - Editing: cell contains data
+     */
     enum EditingState {
         case Default
-        case Selected
-        case Error
-        case Valid
+        case Editing
     }
     
-    private var state: EditingState!
+    private let disposable = DisposeBag()
+    private var type: ProfileEditingCellType?
 
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var textField: JVFloatLabeledTextField!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: - Setup Methods
+    
+    func setupWith(type type: ProfileEditingCellType) {
+        self.type = type
+        self.setupDesignForState(.Default, cellType: type)
     }
     
-    func bindTextField() {
-        let disposable = DisposeBag()
+    private func setupDesignForState(cellState: EditingState?, cellType: ProfileEditingCellType?) {
+        guard let type = cellType, state = cellState else { return }
+        self.iconImageView.image = self.iconForCellType(type, forState: state)
+        self.textField.textColor = self.textColorForState(state)
+        self.textField.userInteractionEnabled = (cellType == .Name)
+        self.textField.floatingLabel.text = cellType?.rawValue.capitalizedString
+        
+        self.bindTextField()
+    }
+    
+    private func bindTextField() {
         self.textField.rx_text
-            .subscribeNext { (text) -> Void in
-            if text.characters.count > 0 {
-                self.state = .Valid
-            } else {
-                self.state = .Default
+            .subscribeNext { [unowned self] (text) -> Void in
+                let state: EditingState = (text.characters.count > 0) ? .Editing : .Default
+                self.setupDesignForState(state, cellType: self.type)
             }
-        }
-        .addDisposableTo(disposable)
+            .addDisposableTo(disposable)
     }
     
-    func iconForCellType(type: ProfileEditingCellType, state selected: Bool) -> UIImage? {
-        let suffix = (selected) ? "Selected" : ""
+    // MARK: - Private Functions
+    
+    private func iconForCellType(type: ProfileEditingCellType, forState state: EditingState) -> UIImage? {
+        let suffix = (state == .Editing) ? "Selected" : ""
         let imageName = type.rawValue + "Icon" + suffix
         return UIImage(named: imageName)
+    }
+    
+    private func textColorForState(state: EditingState) -> UIColor {
+        switch state {
+        case .Default: return UIColor.lightGray()
+        case .Editing: return UIColor.darkGray()
+        }
     }
 }
