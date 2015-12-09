8d0ad8d1bdc8756157184354703f5c8972311dbb
Alexander Shcherbakov <oo.shcherbakov@gmail.com>
2015-12-09 10:11:37 -0500

    Preparation to test table view cells

--- TimeSlowerTests/ProfileEditingCellTests.swift
+++ TimeSlowerTests/ProfileEditingCellTests.swift
@@ -7,10 +7,11 @@
 //
 
 import XCTest
+import UIKit
 @testable import TimeSlower
 
 class ProfileEditingCellTests: XCTestCase {
-
+    
     var fakeController: FakeControllerWithTableView!
     
     override func setUp() {
@@ -19,25 +20,54 @@ class ProfileEditingCellTests: XCTestCase {
         // Setup fake controller
         let storyboard = UIStoryboard(name: "FakeStoryboard",
             bundle: NSBundle(forClass: FakeControllerWithTableView.classForCoder()))
-        
-        let controller = storyboard.instantiateInitialViewController()
+        let controller = storyboard.instantiateViewControllerWithIdentifier("FakeControllerWithTableView")
             as! FakeControllerWithTableView
-        
-        XCTAssertNotNil(controller.view, "View should be loaded")
-        XCTAssertNotNil(controller.tableView, "Table view should be loaded")
         
         self.fakeController = controller
         
-        let realBundle = NSBundle.mainBundle()
-        self.fakeController.tableView
-            .registerNib(UINib(nibName: "ProfileEditingTableViewCell",
-            bundle: realBundle),
-            forCellReuseIdentifier: "ProfileEditingTableViewCell")
-        }
+        XCTAssertNotNil(controller.view, "View should be loaded")
+        XCTAssertNotNil(controller.tableView, "Table view should be loaded")
+
+//        // Setup table view cell
+//        self.fakeController.tableView
+//            .registerNib(UINib(nibName: "ProfileEditingTableViewCell",
+//            bundle: NSBundle(forClass: ProfileEditingTableViewCell.classForCoder())),
+//            forCellReuseIdentifier: "ProfileEditingTableViewCell")
+    }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+    
+    func testDequeueCell() {
+        let cell = self.fakeController.tableView(self.fakeController.tableView,
+            cellForRowAtIndexPath: NSIndexPath(forRow: 0, inSection: 0))
+        XCTAssertNotNil(cell, "Cell should not be nil")
+    }
+    
+    func testSetupNameCell() {
+        
+    }
+    
+    func testSetupCountryCell() {
+        
+    }
+    
+    func testSetupBirthdayCell() {
+        
+    }
+}
 
+extension FakeControllerWithTableView : UITableViewDataSource {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        tableView.registerNib(UINib(nibName: "ProfileEditingTableViewCell", bundle: NSBundle(forClass: ProfileEditingTableViewCell.classForCoder())), forCellReuseIdentifier: "ProfileEditingTableViewCell")
+        
+        return self.tableView.dequeueReusableCellWithIdentifier("ProfileEditingTableViewCell")!
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 }
