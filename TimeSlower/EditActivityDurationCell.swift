2d4e2880760697f513a39e46770a93aa44452b66
Alex <oo.shcherbakov@gmail.com>
2016-02-13 16:59:55 -0500

    Update gitignore

---
+++ TimeSlower/EditActivityDurationCell.swift
@@ -0,0 +1,24 @@
+//
+//  EditActivityDurationCell.swift
+//  TimeSlower
+//
+//  Created by Oleksandr Shcherbakov on 2/13/16.
+//  Copyright © 2016 Oleksandr Shcherbakov. All rights reserved.
+//
+
+import UIKit
+
+class EditActivityDurationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
