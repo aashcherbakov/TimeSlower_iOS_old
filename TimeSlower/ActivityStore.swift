8968b1cd44f8313dc73fc984129f46c6077d8208
Alex <oo.shcherbakov@gmail.com>
2016-10-12 14:30:42 -0400

    Add ActivityStore and make create activity in EditActivityVC

---
+++ TimeSlower/ActivityStore.swift
@@ -0,0 +1,9 @@
+//
+//  ActivityStore.swift
+//  TimeSlower
+//
+//  Created by Oleksandr Shcherbakov on 10/12/16.
+//  Copyright © 2016 Oleksandr Shcherbakov. All rights reserved.
+//
+
+import Foundation
